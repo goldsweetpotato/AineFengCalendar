a9c429b131e6ed196fef7a7e7748c18f1c532e32
goldsweetpotato <56987753+goldsweetpotato@users.noreply.github.com>
2024-10-10 22:35:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,42 @@ from gcsa.recurrence import Recurrence, DAILY, SU, SA
 from google.oauth2 import service_account
 from beautiful_date import Jan, Apr, Sept
 
+# gcsa imports
+from gcsa.event import Event
+from gcsa.google_calendar import GoogleCalendar
+from gcsa.recurrence import Recurrence, DAILY, SU, SA
+from google.oauth2 import service_account
+
+# misc imports
+from beautiful_date import Jan, Apr, Sept, Oct
+import json
+import os
+from datetime import date, datetime
+from beautiful_date import hours
+
+# langchain imports
+from langchain_core.runnables.utils import ConfigurableFieldSpec
+from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.agents.react.agent import create_react_agent
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.tools import Tool, StructuredTool  # Use the Tool object directly
+from langchain_openai import ChatOpenAI
+from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain_community.chat_message_histories import (
+    StreamlitChatMessageHistory,
+)
+from langchain_community.callbacks.streamlit import (
+    StreamlitCallbackHandler,
+)
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI
+from langchain.callbacks.tracers import ConsoleCallbackHandler
+from pydantic import BaseModel, Field
+
 
 # Deviations from documentation:
 # 1- Creating service account and then creating a key for it. (because we the app can't create a server for itself).
