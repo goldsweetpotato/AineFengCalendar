e7709947bb68b633b9a22dc9fd2b293f90d61a88
goldsweetpotato <56987753+goldsweetpotato@users.noreply.github.com>
2024-10-10 22:38:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,15 +64,113 @@ credentials = service_account.Credentials.from_service_account_info(
 # Create the GoogleCalendar.
 calendar = GoogleCalendar(credentials=credentials)
 
-# Get the list of events.
-c = list(calendar.get_events(calendar_id="mflin@bu.edu"))
+### Event listing tool
 
-# It erate through the events and show them.
-for event in c:
-    st.write(event)
+# Parameters needed to look for an event
+class GetEventargs(BaseModel):
+    from_datetime: datetime = Field(description="beginning of date range to retrieve events")
+    to_datetime: datetime = Field(description="end of date range to retrieve events")
 
-# Prompt
+# Define the tool 
+def get_events(from_datetime, to_datetime):
+    events = calendar.get_events(calendar_id="mflin@bu.edu", time_min=from_datetime, time_max=to_datetime)
+    # print(list(events), from_datetime, to_datetime)
+    return list(events)
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+# Create a Tool object 
+list_event_tool = StructuredTool(
+    name="GetEvents",
+    func=get_events,
+    args_schema=GetEventargs,
+    description="Useful for getting the list of events from the user's calendar."
+)
+
+#------------
+
+#-------
+### Event adding tool
+
+# Parameters needed to look for an event
+class AddEventargs(BaseModel):
+    start_date_time: datetime = Field(description="start date and time of event")
+    length_hours: int = Field(description="length of event")
+    event_name: str = Field(description="name of the event")
+
+
+# Define the tool 
+def add_event(start_date_time, length_hours, event_name):
+    start = start_date_time
+    end = start + length_hours * hours
+    event = Event(event_name,
+                  start=start,
+                  end=end)
+    return calendar.add_event(event, calendar_id="mndhamod@gmail.com")
+
+# Create a Tool object 
+add_event_tool = StructuredTool(
+    name="AddEvent",
+    func=add_event,
+    args_schema=AddEventargs,
+    description="Useful for adding an event with a start date, event name, and length in hours the list of events. Calls get_events to make sure if the new event has been deleted."
+)
+
+#------------
+
+##Update this list with the new tools
+tools = [list_event_tool, add_event_tool]
+
+#-----------
+
+
+# Create the LLM
+llm = ChatOpenAI(api_key=st.secrets["OPENAI_API_KEY"], temperature=0.1)
+
+# Messages used by the chatbot
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful Google Calendar assistant"),
+        ("human", "{input}"),
+        # Placeholders fill up a **list** of messages
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+)
+
+
+# Creating the agent that will integrate the provided calendar tool with the LLM.
+agent = create_tool_calling_agent(llm, tools, prompt)
+agent = AgentExecutor(
+    agent=agent, 
+    tools=tools,
+    # verbose=True,
+)
+
+#--------------------
+
+
+# Storing message history
+msgs = StreamlitChatMessageHistory(key="special_app_key")
+
+# Load the first AI message
+if len(msgs.messages) == 0:
+    msgs.add_ai_message("How may I assist you today?")
+
+# Add the rest of the conversation
+for msg in msgs.messages:
+        if (msg.type in ["ai", "human"]):
+                st.chat_message(msg.type).write(msg.content)
+
+# When the user enters a new prompt
+if entered_prompt := st.chat_input("What does my day look like?"):
+    # Add human message
+    st.chat_message("human").write(entered_prompt)
+    msgs.add_user_message(entered_prompt)
+
+    # get a response from the agent
+    st_callback = StreamlitCallbackHandler(st.container())
+    response = agent.invoke({"input": entered_prompt}, {"callbacks": [st_callback, ConsoleCallbackHandler()]})
+
+    # Add AI response.
+    response = response["output"]
+    st.chat_message("ai").write(response)
+    msgs.add_ai_message(response)
+
